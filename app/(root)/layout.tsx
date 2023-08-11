dc80f20972c316b97c8a1915c74081d3aa52eba3
Lorenzo Tomas Diez <lorenzotomasdiez@Lorenzos-Mac-mini.local>
2023-08-10 22:32:31 -0300

    first commit: layout done

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,37 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import '../globals.css'
+import { Inter } from 'next/font/google'
+import { Bottombar, LeftSidebar, RightSidebar, Topbar } from '@/components'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Threads',
+  description: 'A Next.js 13 Meta Threads Application'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Topbar />
+          <main className="flex flex-row">
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>
+                {children}
+              </div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
